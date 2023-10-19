df165304c4436a1ab6bc8d85c943e2e9c6102117
Hebatullah Abdelazem <hebatullah.abdelazem@gizasystems.com>
2023-10-19 12:01:11 +0300

    add stack based linked list

---
+++ Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Stack Based On Linked List
+            Stack.StackBasedOnLinkedList.Stack.Stack stack = new();
+            Console.WriteLine("Is Stack Empty: " + stack.IsEmpty());
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Print();  
+            Console.WriteLine("Item: " + stack.Pop()); 
+            Console.WriteLine("Stack Size: " + stack.Size());
+            stack.Print();
+            Console.WriteLine("Item: " + stack.Peek());
+            Console.WriteLine("Stack Size: " + stack.Size());
+            stack.Print();
+
+            #endregion
+        }
+    }
+}
---
+++ Stack/StackBasedOnLinkedList/Stack/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Stack.StackBasedOnLinkedList.Stack
+{
+    public class Stack
+    {
+        NormalLinkedList.LinkedList s = new();
+        public void Push(int data)
+        {
+            s.InsertFirst(data);
+        }
+
+        public int Pop()
+        {
+            if (s.head == null)
+            {
+                throw new InvalidOperationException("The Stack is empty.");
+            }
+
+            int r = s.head.Data;
+            s.DeleteHeadV2();
+            return r;
+        }
+
+        public int Peek()
+        {
+            if (s.head == null)
+            {
+                throw new InvalidOperationException("The Stack is empty.");
+            }
+
+            return s.head.Data;
+        }
+
+        public int Size()
+        {
+            return s.Length();
+        }
+
+        public bool IsEmpty() 
+        {
+            return s.Length() <= 0;
+        }
+
+        public void Print()
+        {
+            s.PrintList();
+        }
+    }
+}
