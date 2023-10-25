75b4414f2bf7e2393c5da31fb89e221930d800ab
Hebatullah Abdelazem <hebatullah.abdelazem@gizasystems.com>
2023-10-25 17:16:31 +0300

    add FNV_la hash function

---
+++ Hash/Program.cs
@@ -0,0 +1,14 @@
+﻿using Hash.HashFunction;
+
+namespace Hash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FNV_la hash = new ();
+            hash.Hash32("This is Original Text");
+            hash.Hash64("This is Original Text");
+        }
+    }
+}
